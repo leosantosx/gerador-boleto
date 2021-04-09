f5762ece570023b105fdd9a39a5c7a89aae13b92
Leonardo do Nascimento dos Santos <leonardo.nascimento@marche.com.br>
2021-04-09 11:49:32 -0300

    finalizando

--- src/server.js
+++ src/server.js
@@ -1,16 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const routes = require('./routes')
-require('dotenv').config()
 
 const app = express()
 app.use(express.json())
 
-app.use(function(err, req, res, next) {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-});
 app.use(routes)
-
 
 const port = process.env.PORT || 3000
 
